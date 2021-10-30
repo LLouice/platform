b58c18c819c63a3498b9a9a2c61a0c20b1e25b23
llouice <g13920228592@gmail.com>
2021-10-30 22:16:02 +0800

    Update env_logger format

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -17,12 +17,13 @@ macro_rules! init_env_logger {
                 .format(|buf, record| {
                     writeln!(
                         buf,
-                        "{}:{} {} [{}] - {}",
+                        "[{}:{}] <{}> {} - {}",
                         record.file().unwrap_or("unknown"),
                         record.line().unwrap_or(0),
                         chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                        record.level(),
-                        record.args()
+                        // buf.default_styled_level(record.level()),
+                        buf.default_level_style(record.level()).set_bold(true).value(record.level()),
+                        buf.style().set_intense(true).set_bold(true).set_color(env_logger::fmt::Color::Magenta).value(record.args())
                     )
                 })
                 .filter(None, log::LevelFilter::Debug)
