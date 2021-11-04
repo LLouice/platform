005236270665ae4775da4428268f848c0e306262
llouice <g13920228592@gmail.com>
2021-11-04 19:07:52 +0800

    Add train / summary / eval

--- AdaptiveE/ada-rs/src/main.rs
+++ AdaptiveE/ada-rs/src/main.rs
@@ -24,6 +24,33 @@ use tensorflow::ops::{self, create_summary_file_writer, summary_writer, write_su
 #[cfg(feature = "examples_system_alloc")]
 static ALLOCATOR: std::alloc::System = std::alloc::System;
 
+const TRAIN_SIZE: i64 = 43743; // (h,r)
+const VAL_SIZE: i64 = 21121;
+const TEST_SIZE: i64 = 14038;
+
+enum Dev {
+    Val,
+    Test,
+}
+
+struct EvalValue {
+    rank: f32,
+    hit1: f32,
+    hit3: f32,
+    hit10: f32,
+}
+
+impl EvalValue {
+    fn new(rank: f32, hit1: f32, hit3: f32, hit10: f32) -> Self {
+        Self {
+            rank,
+            hit1,
+            hit3,
+            hit10,
+        }
+    }
+}
+
 fn main() -> Result<()> {
     run()
 }
@@ -40,21 +67,6 @@ fn run() -> Result<()> {
         ));
     }
 
-    // Generate some test data.
-    /*
-    let w = 0.1;
-    let b = 0.3;
-    let num_points = 100;
-    let steps = 201;
-    let mut rand = random::default();
-    let mut x = Tensor::new(&[num_points as u64]);
-    let mut y = Tensor::new(&[num_points as u64]);
-    for i in 0..num_points {
-        x[i] = (2.0 * rand.read::<f64>() - 1.0) as f32;
-        y[i] = w * x[i] + b;
-    }
-    */
-
     // Load the computation graph defined by regression.py.
     let mut graph = Graph::new();
     let mut proto = Vec::new();
@@ -68,34 +80,101 @@ fn run() -> Result<()> {
     sess_opt.set_config(config_proto.as_slice())?;
     let session = Session::new(&sess_opt, &graph)?;
 
+    // custom
+    let batch_size_trn: i64 = 4;
+    let batch_size_dev: i64 = 8;
+    // resume from checkpoint
+    let epochs: i64 = 1;
+    let epoch_start: i64 = 1;
+    let steps: i64 = TRAIN_SIZE / batch_size_trn;
+    let repeat_num = epochs * steps;
+    let batch_size_trn_tensor = Tensor::<i64>::new(&[]).with_values(&[batch_size_trn])?;
+    let batch_size_dev_tensor = Tensor::<i64>::new(&[]).with_values(&[batch_size_dev])?;
+    let repeat_num_tensor = Tensor::<i64>::new(&[]).with_values(&[repeat_num])?;
+    let lr: f32 = 0.001;
+    let lr_tensor = Tensor::<f32>::new(&[]).with_values(&[lr])?;
+
+    // train stuff
+    let eval_interval = 5;
+
+    println!("{}", batch_size_trn_tensor);
+
     // get op from graph_def
     // the ops
-    // init / op_optimize / op_file_path / op_save / op_summary
+    // init / placeholder / op_optimize / op_file_path / op_save / op_summary
 
     let op_init = graph.operation_by_name_required("init")?;
+    let op_ph_batch_size_trn = graph.operation_by_name_required("custom/batch_size_trn")?;
+    let op_ph_batch_size_dev = graph.operation_by_name_required("custom/batch_size_dev")?;
+    let op_ph_repeat = graph.operation_by_name_required("custom/repeat")?;
+    let op_ph_lr = graph.operation_by_name_required("custom/lr")?;
+
+    // dataset
     let op_batch_data_trn_init =
         graph.operation_by_name_required("data_trn/dataset_trn/MakeIterator")?;
     let op_batch_data_trn = graph.operation_by_name_required("data_trn/dataset_trn/get_next")?;
-
     let op_trn_record_path = graph.operation_by_name_required("data_trn/dataset_trn/Const")?;
     // based on current dir
     let trn_record_path: Tensor<String> = Tensor::from(String::from("assets/symptom_trn.tfrecord"));
 
+    let op_batch_data_val_init =
+        graph.operation_by_name_required("data_val/dataset_val/MakeIterator")?;
+    let op_batch_data_val = graph.operation_by_name_required("data_val/dataset_val/get_next")?;
+    let op_val_record_path = graph.operation_by_name_required("data_val/dataset_val/Const")?;
+    // based on current dir
+    let val_record_path: Tensor<String> = Tensor::from(String::from("assets/symptom_val.tfrecord"));
+
+    let op_batch_data_test_init =
+        graph.operation_by_name_required("data_test/dataset_test/MakeIterator")?;
+    let op_batch_data_test = graph.operation_by_name_required("data_test/dataset_test/get_next")?;
+    let op_test_record_path = graph.operation_by_name_required("data_test/dataset_test/Const")?;
+    // based on current dir
+    let test_record_path: Tensor<String> =
+        Tensor::from(String::from("assets/symptom_test.tfrecord"));
+
+    // train op
     let op_optimize = graph.operation_by_name_required("optimizer/optimize")?;
     let op_global_step = graph.operation_by_name_required("optimizer/global_step")?;
     let op_loss = graph.operation_by_name_required("loss/loss")?;
 
+    // eval op
+    let op_rank_val = graph.operation_by_name_required("eval_val/rank_val")?;
+    let op_hit1_val = graph.operation_by_name_required("eval_val/hits1_val")?;
+    let op_hit3_val = graph.operation_by_name_required("eval_val/hits3_val")?;
+    let op_hit10_val = graph.operation_by_name_required("eval_val/hits10_val")?;
+    let op_eval_val = graph.operation_by_name_required("eval_val/eval_op_val")?;
+
+    let op_rank_test = graph.operation_by_name_required("eval_test/rank_test")?;
+    let op_hit1_test = graph.operation_by_name_required("eval_test/hits1_test")?;
+    let op_hit3_test = graph.operation_by_name_required("eval_test/hits3_test")?;
+    let op_hit10_test = graph.operation_by_name_required("eval_test/hits10_test")?;
+    let op_eval_test = graph.operation_by_name_required("eval_test/eval_op_test")?;
+
+    // save op
     let op_file_path = graph.operation_by_name_required("save/Const")?;
     let op_save = graph.operation_by_name_required("save/control_dependency")?;
     let file_path_tensor: Tensor<String> = Tensor::from(String::from("checkpoints/saved.ckpt"));
 
+    // summary, not used here
     let op_summary = graph.operation_by_name_required("summaries/summary_op/summary_op")?;
 
     // Load the test data into the session.
     let mut init_step = SessionRunArgs::new();
     init_step.add_target(&op_init);
+    init_step.add_feed(&op_ph_batch_size_trn, 0, &batch_size_trn_tensor);
+    init_step.add_feed(&op_ph_batch_size_dev, 0, &batch_size_dev_tensor);
+    init_step.add_feed(&op_ph_repeat, 0, &repeat_num_tensor);
+    init_step.add_feed(&op_ph_lr, 0, &lr_tensor);
+
     init_step.add_feed(&op_trn_record_path, 0, &trn_record_path);
     init_step.add_target(&op_batch_data_trn_init);
+
+    init_step.add_feed(&op_val_record_path, 0, &val_record_path);
+    init_step.add_target(&op_batch_data_val_init);
+
+    init_step.add_feed(&op_test_record_path, 0, &test_record_path);
+    init_step.add_target(&op_batch_data_test_init);
+
     session.run(&mut init_step)?;
 
     // inspect the batch data
@@ -114,43 +193,107 @@ fn run() -> Result<()> {
     };
     // data_closure()?;
 
-    // Train the model.
-    let train_closure = || -> Result<()> {
-        let epochs = 1;
-        let mut train_step = SessionRunArgs::new();
-        // train_step.add_feed(&op_x, 0, &x);
-        // train_step.add_feed(&op_y, 0, &y);
-        let loss_ix = train_step.request_fetch(&op_loss, 0);
-
-        train_step.add_target(&op_optimize);
-        for _ in 0..epochs {
-            session.run(&mut train_step)?;
-            let loss: Tensor<f64> = train_step.fetch(loss_ix)?;
-            // eprintln!("batch_data:\n\t{:?}", batch_data);
-            eprintln!("loss:\n\t{}", loss);
-        }
+    let save = |ckpt_name: &str| -> Result<()> {
+        let file_path_tensor: Tensor<String> = Tensor::from(String::from(ckpt_name));
+        let mut save_step = SessionRunArgs::new();
+        save_step.add_feed(&op_file_path, 0, &file_path_tensor);
+        save_step.add_target(&op_save);
+        session.run(&mut save_step)?;
         Ok(())
     };
-    // train_closure()?;
 
-    let mut writer = SummaryWriter::new(&("./logs/first".to_string()));
+    let restore = |init_step: &mut SessionRunArgs, ckpt_name: &str| -> Result<()> {
+        session.run(init_step)?;
+        // Load the model.
+        let op_load = graph.operation_by_name_required("save/restore_all")?;
+        let file_path_tensor: Tensor<String> = Tensor::from(String::from(ckpt_name));
+        let mut step = SessionRunArgs::new();
+        step.add_feed(&op_file_path, 0, &file_path_tensor);
+        step.add_target(&op_load);
+        session.run(&mut step)?;
+        Ok(())
+    };
 
-    let epochs = 10;
-    let mut train_step = SessionRunArgs::new();
-    let loss_token = train_step.request_fetch(&op_loss, 0);
-    let global_step_token = train_step.request_fetch(&op_global_step, 0);
-    // let summay_token = train_step.request_fetch(&op_summay, 0);
-    train_step.add_target(&op_optimize);
+    // Train the model.
+    let mut train = || -> Result<()> {
+        let mut writer = SummaryWriter::new(&("./logs/first".to_string()));
+        let mut train_step = SessionRunArgs::new();
+        let loss_token = train_step.request_fetch(&op_loss, 0);
+        let global_step_token = train_step.request_fetch(&op_global_step, 0);
+        train_step.add_target(&op_optimize);
 
-    for _ in 0..epochs {
-        session.run(&mut train_step)?;
-        let loss: f32 = train_step.fetch(loss_token)?[0];
-        eprintln!("loss:\n\t{}", loss);
-        let global_step: i64 = train_step.fetch(global_step_token)?[0];
-        // let summay: Tensor<String> = train_step.fetch(loss_token)?;
-        writer.add_scalar("loss", loss, global_step as usize);
-    }
-    writer.flush();
+        let eval = |dev: Dev, writer: &mut SummaryWriter, global_step: i64| -> Result<EvalValue> {
+            let mut eval_step = SessionRunArgs::new();
+            eval_step.add_feed(&op_ph_batch_size_dev, 0, &batch_size_dev_tensor);
+
+            let (rank_token, hit1_token, hit3_token, hit10_token) = match dev {
+                Dev::Val => {
+                    eval_step.add_target(&op_eval_val);
+                    (
+                        eval_step.request_fetch(&op_rank_val, 0),
+                        eval_step.request_fetch(&op_hit1_val, 0),
+                        eval_step.request_fetch(&op_hit3_val, 0),
+                        eval_step.request_fetch(&op_hit10_val, 0),
+                    )
+                }
+                Dev::Test => {
+                    eval_step.add_target(&op_eval_test);
+                    (
+                        eval_step.request_fetch(&op_rank_test, 0),
+                        eval_step.request_fetch(&op_hit1_test, 0),
+                        eval_step.request_fetch(&op_hit3_test, 0),
+                        eval_step.request_fetch(&op_hit10_test, 0),
+                    )
+                }
+            };
+            session.run(&mut eval_step)?;
+            // fetch
+            let rank: f32 = eval_step.fetch(rank_token)?[0];
+            let hit1: f32 = eval_step.fetch(hit1_token)?[0];
+            let hit3: f32 = eval_step.fetch(hit3_token)?[0];
+            let hit10: f32 = eval_step.fetch(hit10_token)?[0];
+            writer.add_scalar("rank", rank, global_step as usize);
+            writer.add_scalar("hit1", hit1, global_step as usize);
+            writer.add_scalar("hit3", hit3, global_step as usize);
+            writer.add_scalar("hit10", hit10, global_step as usize);
+            println!(
+                "rank: {}, hit1: {} hit3: {}, hit10: {}",
+                rank, hit1, hit3, hit10
+            );
+            writer.flush();
+            let value = EvalValue::new(rank, hit1, hit3, hit10);
+            Ok(value)
+        };
+
+        let mut global_step: i64 = 0;
+        let steps = 100;
+        // for e in 1..=epochs {
+        for e in epoch_start..=5 {
+            // for i in 0..steps {
+            let mut total_loss: f32 = 0.;
+            for i in 0..steps {
+                session.run(&mut train_step)?;
+                let loss: f32 = train_step.fetch(loss_token)?[0];
+                total_loss += loss;
+                // eprintln!("loss:\n\t{}", loss);
+                global_step = train_step.fetch(global_step_token)?[0];
+                writer.add_scalar("loss", loss, global_step as usize);
+            }
+            let loss = total_loss / steps as f32;
+            println!("[{}] loss: {}", e, loss);
+            if e % eval_interval == 0 {
+                let eval_value = eval(Dev::Val, &mut writer, global_step)?;
+                let checkpoint_name = format!(
+                    "checkpoints/rank_{}_loss_{}_epoch_{}_step_{}",
+                    eval_value.rank, loss, e, global_step
+                );
+                save(&checkpoint_name)?;
+            }
+        }
+        writer.flush();
+        Ok(())
+    };
+    train()?;
 
     /*
     // Save the model.
