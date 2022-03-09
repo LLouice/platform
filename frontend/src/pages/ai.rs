6e206c36d052467d1f7728bd29cdfffcd8e54267
llouice <g13920228592@gmail.com>
2022-03-09 17:36:27 +0800

    Add view_table demo

--- frontend/src/pages/ai.rs
+++ frontend/src/pages/ai.rs
@@ -1,10 +1,9 @@
 use web_sys::HtmlElement;
 use yew::prelude::*;
 
-use crate::app::{App, NodeInfo,};
+use crate::app::{App, NodeInfo};
 
 pub struct AI;
-
 
 #[derive(Properties, Clone, PartialEq)]
 pub struct AIProps {
@@ -56,19 +55,36 @@ impl AI {
 
     fn view_table(&self, ctx: &Context<Self>) -> Html {
         html! {
-            <section class="hero is-info is-bold is-large">
-                <div class="hero-body">
-                    <div class="container">
-                        <h1 class="title">
-                            { "AI" }
-                        </h1>
-                        <h2 class="subtitle">
-                            // { format!("预测 {self.label}::{self.name}") }
-                            { ctx.props().node_info.clone() }
-                        </h2>
-                    </div>
-                </div>
-            </section>
+            <div class="columns is-centered mt-3">
+            <table class="table">
+            <thead>
+                <tr>
+                <th><abbr title="Position">{"Pos"}</abbr></th>
+                <th>{ "疾病" }</th>
+                <th>{"Confidence"}</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                <th><abbr title="Position">{"Pos"}</abbr></th>
+                <th>{"疾病"}</th>
+                <th>{ "Confidence "}</th>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr>
+                <th>{ "1" }</th>
+                <td>{ "A" }</td>
+                <td>{ "0.1" }</td>
+                </tr>
+                <tr>
+                <th>{ "2" }</th>
+                <td>{ "B" }</td>
+                        <td>{ "0.9" }</td>
+                </tr>
+            </tbody>
+            </table>
+            </div>
         }
     }
 }
